96336a8326ee4ff38cc28ec8fe982077c4104b73
s2322036 <s2322036@stu.musashino-u.ac.jp>
2024-01-14 03:55:16 +0900

    家のデータをデータベースに格納

--- houseweather.py
+++ houseweather.py
@@ -21,7 +21,7 @@ cur = con.cursor()
 # 3．実行したいSQLを用意する
 # テーブルを作成するSQL
 # CREATE TABLE テーブル名（カラム名 型，...）;
-sql_create_table_houseweather = 'CREATE TABLE houseweather(temperature int, humidity int);'
+sql_create_table_houseweather = 'CREATE TABLE houseweather(temperature_house int, humidity_house int);'
 # 4．SQLを実行する
 cur.execute(sql_create_table_houseweather)
 # 5．必要があればコミットする（データ変更等があった場合）
@@ -37,17 +37,17 @@ cur = con.cursor()
 sql_create_table = """
 CREATE TABLE IF NOT EXISTS houseweather (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    temperature REAL,
-    humidity INTEGER
+    temperature_house REAL,
+    humidity_house INTEGER
 );
 """
 cur.execute(sql_create_table)
 # 3．データの挿入
-sql_insert = "INSERT INTO houseweather (temperature, humidity) VALUES (?, ?)"
+sql_insert = "INSERT INTO houseweather (temperature_house, humidity_house) VALUES (?, ?)"
 # 4．データのリスト
 houseweather_list = [
     (1.2, 64),
-    (0.8, 66),  # humidityの値を入れてください
+    (0.8, 66),  # humidity_houseの値を入れてください
     (0.1, 70),
     (-0.6, 74),
     (0.2, 68),
@@ -83,7 +83,7 @@ import sqlite3
 con = sqlite3.connect("dsprogramming.db")
 c = con.cursor()
     # データベースに挿入
-"INSERT INTO weather(temperature, humidity) VALUES (?, ?)"
+"INSERT INTO weather(temperature_house, humidity_house) VALUES (?, ?)"
 # データベースへの変更をコミット
 con.commit()
 # データベースのコネクションを閉じる
