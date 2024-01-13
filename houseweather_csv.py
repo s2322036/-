818ec4c14b38d193e5eec9133ed5b68571479e21
s2322036 <s2322036@stu.musashino-u.ac.jp>
2024-01-14 03:54:21 +0900

    家のデータをcsv化

---
+++ houseweather_csv.py
@@ -0,0 +1,19 @@
+import sqlite3
+import csv
+
+# データベースに接続
+conn = sqlite3.connect('dsprogramming.db')
+cursor = conn.cursor()
+
+# データの取得（例：テーブル名が 'your_table'）
+cursor.execute('SELECT * FROM houseweather')
+data = cursor.fetchall()
+
+# CSVに変換
+with open('in.csv', 'w', newline='', encoding='utf-8') as csv_file:
+    csv_writer = csv.writer(csv_file)
+    csv_writer.writerow([i[0] for i in cursor.description])  # ヘッダー行を追加
+    csv_writer.writerows(data)
+
+# データベースとの接続を閉じる
+conn.close()
